1827db4d9d72b42b92b6120fd73fc717bdb48edf
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-30 17:44:39 +0300

    template created

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middlewares
+app.use(express.static("public"));
+
+//Routes
 app.get('/', (req, res) => {
-  res.status(200).send('ındex');
+  res.status(200).render('index', {
+     page_name: 'index',
+  });
 });
+app.get('/about', (req, res) => {
+   res.status(200).render('about', {
+      page_name: 'about',
+   });
+})
 
 const port = 3000;
 app.listen(port, () => {
