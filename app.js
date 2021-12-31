026ef919056f60f150daf2c5ca0935a96da8e5a7
RamazanUstuntas <ustuntasramazan@outlook.com>
2021-12-31 20:35:17 +0300

    pages and functions created

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 const express = require('express');
-const ejs = require('ejs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
+//CONNECT DB
+mongoose.connect('mongodb://localhost/smartedu-db')
+.then(()=> {
+  console.log('DB Connected Successfully')
+});
+
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
+
+//Global Variable
+global.userIN = null;
 
 //Middlewares
-app.use(express.static("public"));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'my_keyboard_cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+}))
 
 //Routes
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-     page_name: 'index',
-  });
-});
-app.get('/about', (req, res) => {
-   res.status(200).render('about', {
-      page_name: 'about',
-   });
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
 })
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
