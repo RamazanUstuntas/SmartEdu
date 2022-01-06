a3586f9c328fb0f47045a7002bdc8ec5df9ca0ba
RamazanUstuntas <ustuntasramazan@outlook.com>
2022-01-06 18:30:09 +0300

    make it better

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(
     secret: 'my_keyboard_cat',
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+    store: MongoStore.create({ mongoUrl: 'mongodb+srv://dbUser:RGHqA7pHZJEQbGK@cluster0.5sswl.mongodb.net/smartedu-db?retryWrites=true&w=majority' }),
   })
 );
 app.use(flash());
